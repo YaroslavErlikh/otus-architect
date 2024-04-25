1be5a8bb4d6bf3610886f5f7004f1c0226ab3ee9
Ярослав Эрлих <i.erlikh@rt-solar.ru>
2024-04-25 20:01:49 +0500

    6

---
+++ src/main/java/erlikh/yaroslav/spacebattle/model/ModelConstants.groovy
@@ -0,0 +1,10 @@
+package erlikh.yaroslav.spacebattle.model
+
+class ModelConstants {
+
+    public static final String POSITION = 'position'
+    public static final String VELOCITY = 'velocity'
+    public static final String DIRECTION = 'direction'
+    public static final String DIRECTIONS_NUMBER = 'directionsNumber'
+    public static final String ANGLE = 'angle'
+}
